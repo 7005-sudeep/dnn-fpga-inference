d77491ff80e5670d3de71bab18ad95098e2d051f
Sudeep - Kakade <saikakade501@gmail.com>
2026-05-26 23:33:26 -0400

    relu.sv

--- rtl/relu.sv
+++ rtl/relu.sv
@@ -1,1 +1,19 @@
+module relu (
+    input  logic clk,
+    input  logic rst_n,
+    input  logic signed [31:0] data_in,
+    output logic signed [31:0] data_out
+);
 
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            data_out <= 32'sd0;
+        else begin
+            if (data_in[31] == 1'b1)
+                data_out <= 32'sd0;
+            else
+                data_out <= data_in;
+        end
+    end
+
+endmodule
