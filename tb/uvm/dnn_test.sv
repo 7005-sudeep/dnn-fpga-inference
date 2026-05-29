6133455356051b0d8a6746dbe227f12cdd6be8a1
Sudeep - Kakade <saikakade501@gmail.com>
2026-05-28 20:36:40 -0400

    dnn_test.sv

---
+++ tb/uvm/dnn_test.sv
@@ -0,0 +1,25 @@
+class dnn_test extends uvm_test;
+    `uvm_component_utils(dnn_test)
+
+    dnn_env env;
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        env = dnn_env::type_id::create("env", this);
+    endfunction
+
+    task run_phase(uvm_phase phase);
+        dnn_sequence seq;
+        phase.raise_objection(this);
+
+        seq = dnn_sequence::type_id::create("seq");
+        seq.start(env.agent.sequencer);
+
+        phase.drop_objection(this);
+    endtask
+
+endclass
